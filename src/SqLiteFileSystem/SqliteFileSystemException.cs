fe59f111d76d59fb8eee0739c4075dc218ec5858
Anthony Truskinger <anthony.truskinger@gmail.com>
2017-11-13 16:56:45 +1000

    Initial checkin of code for SqliteFileSystem

---
+++ src/SqLiteFileSystem/SqliteFileSystemException.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Zio.FileSystems.Community
+{
+    using System;
+
+    public class SqliteFileSystemException : Exception
+    {
+        public SqliteFileSystemException(string message) : base(message)
+        {
+        }
+
+        public SqliteFileSystemException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
